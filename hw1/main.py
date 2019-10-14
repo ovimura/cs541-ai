0f02e1857a259e5c09252f985e0273d29bff1473
Ovidiu Mura <mura@pdx.edu>
2019-10-13 22:56:26 -0700

    adding new classes

--- hw1/main.py
+++ hw1/main.py
@@ -3,6 +3,8 @@ n = 0
 hosts = []
 guests = []
 
+preference_matrix = []
+
 r1 = []
 r2 = []
 
@@ -33,38 +35,47 @@ def print_content(a):
         print "%s" % a[i][:-1]
         i += 1
 
-def process_rows(data):
+def process_preference_matrix(data):
     global n
-    global r1
-    global r2
+    global preference_matrix
     global guests
     r1 = []
-    for k in range(n):
+    for k in range(1,n+1):
         row = data[k].split(' ')
         row[-1] = row[-1].replace('\n', '')
-        r1.append(row)
-#        for j in range(1, n/2+1):
-#            r2[k].append((guests[j-1],data[k].replace('\n', ' ').split(' ')[n/2+j-1]))
+        preference_matrix.append(row)
 
 
 class Person:
-    def __init__(self):
-        self.no = 0
-        self.seat = 0
+    def __init__(self, no, seat):
+        self.no = no
+        self.seat = seat
 
-    def r(p):
+
+class Table:
+    def __init__(self):
+        self.row1 = []
+        self.row2 = []
+
+    def r(self, p):
         global hosts
         if p.no in hosts:
             return 0
         else:
             return 1
 
+    def h(self, p1, p2):
+        global preference_matrix
+        if p1.no == 0 or p2.no == 0:
+            raise Exception("error: incorrect person number")
+        return preference_matrix[p1.no-1][p2.no-1]
+
+
 def main():
     global hosts
     global guests
     global n
-    global r1
-    global r2
+    global preference_matrix
     d = read("data/hw1-inst1.txt")
     print_content(d)
     get_n(d)
@@ -72,14 +83,18 @@ def main():
     print hosts
     get_guests(d)
     print guests
-    process_rows(d)
-    for a in range(len(r1)):
-        print r1[a]
+    process_preference_matrix(d)
+    for a in range(len(preference_matrix)):
+        print preference_matrix[a]
 
-
+    t = Table()
+    p = Person(2,4)
+    print p.no
+    print t.r(Person(1,1))
+    print t.h(Person(1,1), Person(1,2))
 #    print r2
-    read("data/hw1-inst2.txt")
-    read("data/hw1-inst3.txt")
+#    read("data/hw1-inst2.txt")
+#    read("data/hw1-inst3.txt")
 
 
 if __name__ == "__main__":
