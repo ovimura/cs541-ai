82943f62671e756be89fc20c6f173ff8788cdc1c
Ovidiu Mura <ovymura@yahoo.com>
2019-11-13 14:47:30 -0800

    adding solution for orig set

--- hw2/hw2.py
+++ hw2/hw2.py
@@ -1,11 +1,13 @@
 # HW2
 
+import math
+
 class nbayes:
     def __init__(self, url_train, url_test):
         self.F = []
         self.N = []
         self.train = []
-        self.stest = []
+        self.test = []
         with open(url_train) as f:
             t = f.readlines()
             for x in range(len(t)):
@@ -15,8 +17,7 @@ class nbayes:
             t = f.readlines()
             for x in range(len(t)):
                 a = t[x].replace('\n','').split(',')
-                self.stest.append(a)
-
+                self.test.append(a)
 
     def c(self,i):
         '''
@@ -26,7 +27,6 @@ class nbayes:
         '''
         return len([x for x in self.train if int(x[0]) == i])
 
-
     def pr_h(self, i):
         '''
         It calculates the probability of the given hypothesis.
@@ -34,7 +34,6 @@ class nbayes:
         :return:
         '''
         return float(self.c(i)/len(self.train))
-
 
     def pr_f_h(self, j, k, i):
         '''
@@ -45,7 +44,6 @@ class nbayes:
         :return:
         '''
         return float((self.c(i) + self.f(j,k))/self.c(i))
-
 
     def f(self,j,k):
         '''
@@ -60,24 +58,55 @@ class nbayes:
                 fj += 1
         return fj
 
-
     def learn(self):
         self.N = [0,0]
-        fs = [0 for _ in range(len(self.train[0][1:]))]
-        self.F = [fs,fs]
-        print((self.N))
-        print(self.F)
-        print(len(self.F[0]))
-        print(len(self.F[1]))
+        fs1 = [0 for _ in range(1,len(self.train[0][0:]))]
+        fs2 = [0 for _ in range(1,len(self.train[0][0:]))]
+        self.F = [fs1,fs2]
+        for t in self.train:
+            self.N[int(t[0])] += 1
+            for j in range(1,len(t)):
+                if int(t[j]) == 1:
+                    self.F[int(t[0])][j-1] += 1
+        return (self.N, self.F)
 
+    def L(self, c):
+        L = [0,0]
+        for i in [0,1]:
+            L[i] = math.log(self.N[i] + 0.5) - math.log(self.N[0] + self.N[1] + 0.5)
+            for j in range(1,len(c)):
+                s = self.F[i][j-1]
+                if int(c[j]) == 0:
+                    s = self.N[i] - s
+                L[i] = L[i] + math.log(s+0.5) - math.log(self.N[i] + 0.5)
+        return L
+
+    def classify(self, c):
+        L = self.L(c)
+        if L[1] > L[0]:
+            return 1
+        return 0
 
 def main():
     nb = nbayes('data/spect-orig.train.csv', 'data/spect-orig.test.csv')
-    print(nb.pr_h(0))
-    print(nb.pr_h(1))
-    print(nb.pr_f_h(1,0,1))
-    print(nb.pr_f_h(1,1,1))
-
+    a, b = nb.learn()
+    predicted_list = []
+    actual_list = []
+    one_pred_true = 0
+    zeros_pred_true = 0
+    for i in range(len(nb.test)):
+        d = nb.classify(nb.test[i])
+        actual_list.append(int(nb.test[i][0]))
+        if d == 1:
+            predicted_list.append(1)
+        else:
+            predicted_list.append(0)
+    for x in range(len(predicted_list)):
+        if predicted_list[x] == actual_list[x] and predicted_list[x] == 1:
+            one_pred_true += 1
+        if predicted_list[x] == actual_list[x] and predicted_list[x] == 0:
+            zeros_pred_true += 1
+    print("orig {}/{}({})".format(one_pred_true+zeros_pred_true, len(nb.test), round(float((one_pred_true+zeros_pred_true)/len(nb.test)),2)))
 
 if __name__ == "__main__":
     main()
